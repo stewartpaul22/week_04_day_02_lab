1216accfb5a5668f4cce122b54f4eead4fd04c03
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-02-13 17:48:05 +0000

    Initial commit

---
+++ pizza_controller.rb
@@ -0,0 +1,42 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative( './models/pizza_order.rb' )
+
+# index
+get "/pizza-orders" do
+  @orders = PizzaOrder.all()
+  erb(:index)
+end
+
+#create
+post "/pizza-orders" do
+  @order = PizzaOrder.new(params)
+  @order.save()
+  erb(:create)
+end
+
+# get
+get "/pizza-orders/new" do
+  erb(:new)
+end
+
+# show
+get "/pizza-orders/:id" do
+  @order = PizzaOrder.find(params[:id])
+  erb(:show)
+end
+
+# delete
+post "/pizza-orders/:id/delete" do
+  @order = PizzaOrder.find(params[:id])
+  @order.delete()
+  redirect to("/pizza-orders")
+  # erb(:delete)
+end
+
+# edit
+get "/pizza-orders/:id/edit" do
+  @order = PizzaOrder.find(params[:id])
+  erb(:edit)
+end
